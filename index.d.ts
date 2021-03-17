631342ce781f19b9d0dff1b0b204e3b0c82f2437
George Payne <George-Payne@users.noreply.github.com>
2021-03-17 13:24:25 +0100

    feat: add type definitions (#21)

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare namespace JsonSchemaToOpenapiSchema {
+  interface Options {
+    cloneSchema?: boolean;
+    dereference?: boolean;
+  }
+}
+declare function JsonSchemaToOpenapiSchema<T = Record<string | number, any>>(
+  schema: Record<string | number, any>,
+  options?: JsonSchemaToOpenapiSchema.Options
+): T;
+export = JsonSchemaToOpenapiSchema;
