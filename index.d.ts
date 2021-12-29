90c99dca3896474349aa7cdc092fbd91f4f2883d
EmilIvanov <emil.ivanov@mentormate.com>
2021-12-29 21:13:24 +0200

    fix: change converter return type to a promise (#23)

--- index.d.ts
+++ index.d.ts
@@ -7,5 +7,5 @@ declare namespace JsonSchemaToOpenapiSchema {
 declare function JsonSchemaToOpenapiSchema<T = Record<string | number, any>>(
   schema: Record<string | number, any>,
   options?: JsonSchemaToOpenapiSchema.Options
-): T;
+): Promise<T>;
 export = JsonSchemaToOpenapiSchema;
